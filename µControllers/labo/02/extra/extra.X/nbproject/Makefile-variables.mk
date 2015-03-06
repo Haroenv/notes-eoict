600274d2fe5567fe1a46071b5c5a5c73162491cb
Haroenv <fingebimus@me.com>
2015-03-06 23:39:25 +0100

    labo

---
+++ µControllers/labo/02/extra/extra.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=extra.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/extra.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=extra.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/extra.x.tar
