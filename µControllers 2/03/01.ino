b6ba3608c89699b537bcb8d96f9c641ca7ee0153
Haroen Viaene <fingebimus@me.com>
2015-10-07 08:38:55 +0200

    Update 01.ino

--- µControllers 2/03/01.ino
+++ µControllers 2/03/01.ino
@@ -1,12 +1,20 @@
-#define K1 9;
-#define K2 10;
-#define K3;
-void setup() {
-  // put your setup code here, to run once:
+#define K1 9
+#define K2 10
+#define K3 11
 
+void setup() {
+  pinMode(K1,INPUT_PULLUP);
+  pinMode(K2,INPUT_PULLUP);
+  pinMode(K3,INPUT_PULLUP);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int k1 = digitalRead(K1);
+  int k2 = digitalRead(K2);
+  int k3 = digitalRead(K3);
+  if (k1 == LOW || k2 == LOW || k2 == LOW ) {
+    Serial.println('a button is pressed');
+  };
+  delay(1000);
 }
