770add27f242a77e693a8d55bb864b62bb6c9a6c
Haroen Viaene <hello@haroen.me>
2015-10-14 09:37:38 +0200

    majyaaaaaah

---
+++ µControllers 2/04/04.ino
@@ -0,0 +1,23 @@
+#define K1 9
+#define K2 10
+#define K3 11
+const byte ledArray [] = {2, 3, 4, 5};
+const byte led = 2;
+
+void setup() {
+  pinMode(K1,INPUT_PULLUP);
+  pinMode(K2,INPUT_PULLUP);
+  pinMode(K3,INPUT_PULLUP);
+  for (int i = 0; i < 5; i++) {
+    pinMode(ledArray[i],OUTPUT);
+  }
+}
+
+void loop() {
+  if (digitalRead(K3) == LOW) {
+    delay(1000);
+    if(digitalRead(K3) == LOW){
+
+    }
+  }
+}
