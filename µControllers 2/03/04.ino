d73bfa0cc069271f4422645f0eca26f480b60b7e
Haroen Viaene <hello@haroen.me>
2015-10-14 08:31:17 +0200

    mayeah

--- µControllers 2/03/04.ino
+++ µControllers 2/03/04.ino
@@ -2,7 +2,6 @@
 * Dwight Van Der Velpen
 */
 
-
 #define K1  9
 #define K2  10
 #define K3  11
@@ -13,14 +12,14 @@ byte onOff = 1; //0=stop 1=left 2=right
 
 void setup() {
   pinMode ( K1 , INPUT_PULLUP );
-  pinMode ( K2 , INPUT_PULLUP ); 
-  pinMode ( K3 , INPUT_PULLUP ); 
+  pinMode ( K2 , INPUT_PULLUP );
+  pinMode ( K3 , INPUT_PULLUP );
   pciSetup(K1);
   pciSetup(K2);
   pciSetup(K3);
 
   for(int x=0; x<=3;x++) pinMode(ledArray[x],OUTPUT);
-  
+
 }
 
 void pciSetup ( byte pin ){
@@ -29,7 +28,7 @@ void pciSetup ( byte pin ){
   PCICR |= bit ( digitalPinToPCICRbit ( pin ) ) ; // enable interrupt for the group
 }
 
-ISR ( PCINT0_vect ){    
+ISR ( PCINT0_vect ){
     if((!digitalRead(K1)) && (digitalRead(K2))&&(digitalRead(K3))){
       while(!digitalRead(K1)){//antiDender
       }
@@ -47,15 +46,15 @@ ISR ( PCINT0_vect ){
     }
 }
 
-void loop() {      
+void loop() {
   for(int i = 0; i < 4; i++) digitalWrite(ledArray[i], bitRead(loper,i));
 
   if((loper == 1) && onOff == 2) loper = 8;
-  else if((loper == 8) && onOff == 1) loper = 1;    
+  else if((loper == 8) && onOff == 1) loper = 1;
   else if(onOff != 0){
     if(onOff == 1) loper*=2;
     else loper/=2;
   }
-  
+
   delay(500);
 }
