b02a0d2bb7e8c8e6a94c67b8a5d6d3e3de583861
Haroen Viaene <hello@haroen.me>
2015-10-14 09:46:23 +0200

    denderrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr

--- µControllers 2/04/04.ino
+++ µControllers 2/04/04.ino
@@ -1,6 +1,7 @@
 #define K1 9
 #define K2 10
 #define K3 11
+
 const byte ledArray [] = {2, 3, 4, 5};
 const byte led = 2;
 
@@ -17,7 +18,11 @@ void loop() {
   if (digitalRead(K3) == LOW) {
     delay(1000);
     if(digitalRead(K3) == LOW){
-
+      for (int i = 0; i < 5; i++) {
+        digitalWrite(ledArray[i],!digitalRead(ledArray[i]));
+      }
+    } else {
+      digitalWrite(led, !digitalRead(led));
     }
   }
 }
