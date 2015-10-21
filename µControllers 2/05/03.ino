97628af3cf9ee61e939e32e0b011c865312dc6e9
Haroen Viaene <hello@haroen.me>
2015-10-21 09:42:22 +0200

    shitty memes

--- µControllers 2/05/03.ino
+++ µControllers 2/05/03.ino
@@ -1,9 +1,22 @@
+/**
+* not finished
+**/
+
 byte LEN = 5;
 word temperatuur[]={0x3B47,0x0C87,0x000F,0x0007,0xF387};
 
 void setup() {
     Serial.begin(9600);
     for (int i = 0; i < LEN; i++) {
-        Serial.println(temperatuur[i]);
+        Serial.println(getSign(temperatuur[i]));
     }
+}
+
+void loop() {
+
+}
+
+String getSign(word toParse) {
+  if (toParse >> 15 & 1 == 0) return "+";
+  if (toParse >> 15 & 1 == 1) return "-";
 }
