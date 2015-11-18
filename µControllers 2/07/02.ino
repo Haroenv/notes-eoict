b7a0cf105cb8075f9bb207adc971f98833a02884
Haroen Viaene <fingebimus@me.com>
2015-11-18 09:49:28 +0100

    Create 02.ino

---
+++ µControllers 2/07/02.ino
@@ -0,0 +1,46 @@
+char input;
+const byte leds[] = {2, 3, 4, 5};
+byte number;
+char mode;
+
+void setup() {
+  Serial.begin(115200, SERIAL_8O2);
+  Serial.println("Toets de letter in van uw keuze:");
+  Serial.println("a. alle LED's aan");
+  Serial.println("b. alle LED's uit");
+  Serial.println("c. individuele LED aansturen");
+  for (byte i = 0; i < sizeof(leds) / sizeof(byte); i++) {
+    pinMode(leds[i], OUTPUT);
+  }
+}
+
+void loop() {
+  input = Serial.read();
+  if (input == 'a') {
+    Serial.println("geef me een aight");
+    for (byte i = 0; i < sizeof(leds) / sizeof(byte); i++) {
+      digitalWrite(leds[i], HIGH);
+    }
+  } else if (input == 'b') {
+    Serial.println("geef me een baight");
+    for (byte i = 0; i < sizeof(leds) / sizeof(byte); i++) {
+      digitalWrite(leds[i], LOW);
+    }
+  } else if (input == 'c') {
+    Serial.println("geef het nummer van de LED gevolgd door A (aan) of U (uit), bv. 2A:");
+    while (Serial.available() == 0) {}
+    Serial.println("in c");
+    number = (byte)Serial.read() - 49;
+    Serial.println(number);
+    if (number > 0 && number < 5) {
+      Serial.println("in number");
+      Serial.println(number);
+      while (Serial.available() == 0) {}
+      mode = Serial.read();
+      Serial.println(mode);
+      if (mode == 'U' || mode == 'A') {
+        digitalWrite(leds[number], mode == 'U' ? LOW : HIGH);
+      }
+    }
+  }
+}
