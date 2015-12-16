eb06685ac8b157e64b1d9abf68d4ca23ef972d84
Haroen Viaene <fingebimus@me.com>
2015-12-16 09:41:39 +0100

    alsjeblieft jongens!

--- µControllers 2/10/04.ino
+++ µControllers 2/10/04.ino
@@ -1,19 +1,16 @@
 #include <avr/pgmspace.h>
 const char progmemString[] PROGMEM  = {"TEKST HIER ;) ;)"};
-int k;
 char myChar;
 
 void setup() {
   Serial.begin(9600);
 
   int len = strlen_P(progmemString);
-  for (k = 0; k < len; k++) {
-    myChar =  pgm_read_byte_near(progmemString + k);
+  for (int i = 0; i < len; i++) {
+    myChar =  pgm_read_byte_near(progmemString + i);
     Serial.print(myChar);
   }
 
   Serial.println();
 }
-
-void loop() {
-}
+void loop(){}
